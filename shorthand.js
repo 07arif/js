8a303e1586b055cc89aef574b5018bad7c762da6
07arif <ai.arif0711@gmail.com>
2024-01-02 21:59:26 +0600

    methmatical operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,47 @@
+//declaration:
+var tomatoPrice = 20;
+//value update:
+tomatoPrice = 50;
+
+// console.log(tomatoPrice);
+
+var price1 = 90;
+var price2 = 40;
+
+// console.log(price1);
+
+// add some value to the existing value
+price1 = price1 + 10;
+
+// console.log(price1);
+
+// addition to the same variable shorthand.
+price1 += 10;
+
+// console.log(price1);
+
+// subtraction :
+price1 -= 4;
+
+// console.log(price1);
+
+// multiplication & division
+
+price1 *= 10;
+// console.log(price1);
+
+price1 /= 40;
+// console.log(price1);
+
+// adding one value:
+var age1 = 15;
+age1++;
+console.log(age1);
+//   ++ means adding 1 to the existing
+
+// removing one value:
+var age2 = 15;
+age2--;
+console.log(age2);
+
+// --means you are deducting 1 from the existing
