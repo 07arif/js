dcc0e8c4f61c7a0ffc482b8d9b0dbb105322b283
07arif <ai.arif0711@gmail.com>
2024-01-03 22:14:11 +0600

    variable types tofixed parsefloat and remainder

---
+++ remainder.js
@@ -0,0 +1,5 @@
+var mangoes = 10;
+var person = 4;
+
+// console.log(mangoes / person); ## eta vagfol
+// console.log(mangoes % person); ## eta vasesh.
---
+++ types.js
@@ -0,0 +1,23 @@
+//Typeof:
+
+var price = 100;
+var price1 = "100";
+
+// console.log(typeof price);
+// console.log(typeof price1);
+
+var name1 = "ariana";
+var isHappy = true;
+
+// console.log(typeof isHappy);
+// console.log(typeof name1);
+
+// toFixed:
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+sum = parseFloat(sum);
+console.log(sum);
+console.log(typeof sum);
