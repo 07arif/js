f8506322e7d3a1294756195778fc90bb8f0048ed
07arif <ai.arif0711@gmail.com>
2024-01-04 20:06:22 +0600

    declare arra, array length and array index

---
+++ array and conditionals/array.js
@@ -0,0 +1,22 @@
+var friendsAge = [11, 21, 45, 17, 40];
+var friendsName = ["mahi", "shabnur", "sakib", "kollol"];
+var picnicFee = [500, 200, 600, 300];
+var vowels = ["a", "e", "i", "o", "u"];
+
+// Array এর ভিতর কয়টা উপাদান আছে তা বের করতে হলে ঃ
+console.log(friendsAge.length);
+
+Array এর ভিতর উপাদান শুরু হয় শুন্য দিয়ে । 
+
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// get element by index
+
+console.log(numbers);
+
+///for specfic index
+console.log(numbers[0]);
+
+// using var for define index
+var element = numbers[1];
+console.log(element);
