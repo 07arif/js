3152ce41613efe60a60bfe0af183debaf248a05a
07arif <ai.arif0711@gmail.com>
2024-01-05 23:03:12 +0600

    add or remove element from array using push, pop

---
+++ array and conditionals/push-pop.js
@@ -0,0 +1,19 @@
+var numbers = [23, 34, 54, 65, 76, 87];
+console.log(numbers);
+
+// use push to add element to an array as the last element of the array.
+numbers.push(300);
+console.log(numbers);
+
+// remove an element from array.
+numbers.pop;
+
+var friends = ["balam", "kalam", "salam"];
+
+friends.push("gelam");
+friends.push("pailam");
+
+console.log(friends);
+
+var element = friends.pop();
+console.log(element);
