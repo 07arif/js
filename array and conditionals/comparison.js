339d7fe402c4f511a18c52d58772995efb5ce279
07arif <ai.arif0711@gmail.com>
2024-01-06 21:02:02 +0600

    compare variable and comparison operator

---
+++ array and conditionals/comparison.js
@@ -0,0 +1,35 @@
+// comparison
+/*
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(5 != 6);
+console.log(5 != 5);
+
+//less than or equal:
+console.log(5 <= 6);
+console.log(6 <= 6);
+console.log(16 <= 6);
+
+//Grater than or equal:
+console.log(5 >= 6);
+console.log(5 >= 2);
+*/
+
+//
+
+// multiple conditions
+// money1 > money2 && result1 > result2;
+
+// money1 > money2 || result1 > result2;
+
+// how condition work
+var iphone = 7000;
+var myBudget = 5000;
+
+if (iphone < myBudget) {
+  console.log("I will buy Iphone");
+} else {
+  console.log("i don't have money ");
+}
