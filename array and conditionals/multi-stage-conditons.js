0954f6b8a75d928e4cc067dc48b4eac617ce184b
07arif <ai.arif0711@gmail.com>
2024-01-07 11:45:29 +0600

    multi stage condition and nested conditions

---
+++ array and conditionals/multi-stage-conditons.js
@@ -0,0 +1,14 @@
+var money = 10;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if (danishPrice < money) {
+  console.log("Dan dan Danish Khamu");
+} else if (butterBread < money) {
+  console.log("ButterBread khamo");
+} else if (toastBiscuit < money) {
+  console.log("toast Khamu");
+} else {
+  console.log("ca ei khamu");
+}
