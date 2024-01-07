6ff09289608601d5967daad3a4fa844e112cc1f5
07arif <ai.arif0711@gmail.com>
2024-01-07 11:29:43 +0600

    multiple condition and or

---
+++ array and conditionals/multiple-conditions.js
@@ -0,0 +1,23 @@
+var isGraduated = true;
+var salary = 100000;
+var cars = 0;
+
+/*
+if (isGraduated == true) {
+  console.log("esho biye kore feli");
+} else {
+  console.log("tor kopale biye nai");
+}
+*/
+
+// if (isGraduated == true && salary > 5000 && cars >= 1) {
+//   console.log("esho biye kore feli");
+// } else {
+//   console.log("tor kopale biye nai");
+// }
+
+if ((isGraduated === true && salary > 50000) || cars >= 1) {
+  console.log("esho prem kori");
+} else {
+  console.log("tor friend er number de");
+}
