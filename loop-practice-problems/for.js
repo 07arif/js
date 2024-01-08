a1d505da731f000ece4eed9bb789d39133656b5b
07arif <ai.arif0711@gmail.com>
2024-01-08 08:13:24 +0600

    for loop how for loop work

---
+++ loop-practice-problems/for.js
@@ -0,0 +1,45 @@
+/*
+// while loop:
+
+var rostGiven = 0;
+while (rostGiven < 10) {
+  console.log("rost de, please");
+  rostGiven++;
+}
+
+*/
+
+/*
+// for loop:
+
+
+for (var rostGiven = 0; rostGiven < 10; rostGiven++) {
+  console.log("Rost den , Please");
+}
+
+*/
+
+/*
+// simple version of for loop:
+
+console.log("Simple for loop");
+for (var i = 0; i < 7; i++) {
+  console.log(i);
+}
+
+*/
+
+// for loop
+for (var i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// even
+for (var i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+// odd
+for (var i = 1; i <= 10; i += 2) {
+  console.log(i);
+}
