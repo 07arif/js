b981d3dca7beddbab76b5e44b765637d94779d09
07arif <ai.arif0711@gmail.com>
2024-01-08 08:36:31 +0600

    run a loop for each element of an array

---
+++ loop-practice-problems/array-loop.js
@@ -0,0 +1,21 @@
+/*
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+*/
+
+// Display every element of an array:
+var numbers = [45, 55, 65, 75, 85, 25, 15, 35];
+
+for (var i = 0; i < 6; i++) {
+  var number = numbers[i];
+  console.log(number);
+}
+// ekhane array er length uncountable o hote pare tai array.length use korle jotogula element ache show korbe
+
+var tableItems = ["speacker", "sunglass", "meril", "cum", "ssd", "box"];
+
+for (var i = 0; i < tableItems.length; i++) {
+  var item = tableItems[i];
+  console.log(item);
+}
