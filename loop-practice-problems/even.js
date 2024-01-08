d8a85040e0bfb68499a84ac6df44e7bbe8f84270
07arif <ai.arif0711@gmail.com>
2024-01-08 07:52:26 +0600

    loop example numbers, odd numbers, even numbers

---
+++ loop-practice-problems/even.js
@@ -0,0 +1,13 @@
+// শুন্য থেকে শুরু করলে ১০ পর্যন্ত ৯ টি সংখ্যা দেখায় কারন সে শুন্যকে কাউন্ট করে।
+var number = 1;
+while (number < 10) {
+  //   console.log(number);
+  number++;
+}
+
+// target 1 to 10 all even numbers;
+var number = 0;
+while (number <= 10) {
+  console.log(number);
+  number += 2;
+}
