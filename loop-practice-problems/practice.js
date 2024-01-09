73438f6e7ecd399c67f899e18531f073c34bbae8
07arif <ai.arif0711@gmail.com>
2024-01-09 07:21:49 +0600

    remainder practice problem

---
+++ loop-practice-problems/practice.js
@@ -0,0 +1,22 @@
+/*
+sarah's mother is teaching her mathematics. 
+She gave sarah a number and asked her what the remainder would be if 
+she divided the number by 5. Help sarah write the program.
+
+
+input:
+the first line of the contains the number.
+
+Output:
+Print the Remainder.
+
+                Sample Input:
+                119
+                Output:
+                4
+*/
+
+var num = 119;
+
+var remainder = num % 5;
+console.log(remainder);
