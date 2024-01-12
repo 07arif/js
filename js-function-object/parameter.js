95314f68edd224ad3d5c9584bfab2e93f301aaff
07arif <ai.arif0711@gmail.com>
2024-01-12 15:11:10 +0600

    add function parameter, handle multiple parameter

--- js-function-object/parameter.js
+++ js-function-object/parameter.js
@@ -4,4 +4,34 @@ function bringSingara(money) {
   console.log(money);
 }
 // parameter er value dete hobe
-bringSingara(100);
+// bringSingara(100);
+
+// parameter er nam dite hobe
+function bringSomosa(money) {
+  console.log("Taka disen:", money);
+  console.log("ai nen singara");
+}
+// parameter er value dete hobe/ perameter er value variable a diye variable er nam dile hbe.
+var taka = 450;
+// bringSomosa(taka);
+
+// multi parameter:
+function add(num1, num2) {
+  console.log("going to add:", num1, num2);
+}
+// parameter er value na dile default hisabe undifined dehabe.
+// add();
+// add(50);
+
+// add(609, 89);
+
+function sum(a, b, c, d, e, f) {
+  console.log(a, b, c, d, e, f);
+  var sum = a + b + c + d + e + f;
+  console.log(sum);
+}
+
+// sum();
+// sum(1, 2, 3, 4, 5);
+
+sum(1, 2, 3, 4, 5, 6, 7);
