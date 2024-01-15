8f65127838bd862d6e49dd93227c9a4638e69f2d
07arif <ai.arif0711@gmail.com>
2024-01-15 13:27:44 +0600

    js concepts recap, var let, and const

---
+++ apply-js-core-concepts/recap.js
@@ -0,0 +1,51 @@
+// 1.Variable
+var myName = 'khan';
+
+// math operation: +, -, *, /
+// shorthand: +=, --, *=, /=
+// ++, --
+
+// 2. Array
+var friends = ['abul', 'kabul', 'navul', 'jabul']:
+
+var thirdFriend = friends[2];
+friends[3] = 'dambool';
+
+
+// 3. Conditionals
+// >, <, >=, <=, ==, ===, !=, !==
+
+if (friends.length < 2) {
+    console.log('fokir tor kono friend nai');
+} else {
+    console.log('ache')
+}
+
+// 4. loop
+var number = 0;
+while (number <= 6) {
+    console.log(number)
+    number++;
+}
+
+for (var i = 0; i <= 10; i++){
+    console.log('job den')
+}
+
+// 5.function:
+function isMoonUp(time) {
+    if (time > 7) {
+        return true;
+     }
+}
+ 
+// 6. object
+var student = {
+    Name: 'arif',
+    class: 'throw',
+    roll: 40,
+    address: 'ctg'
+}
+
+
+
---
+++ apply-js-core-concepts/var-let-const.js
@@ -0,0 +1,9 @@
+// var
+
+// let
+let age = 11;
+age = 22;
+
+// const
+const countryName = "Bangladesh";
+console.log(age, countryName);
