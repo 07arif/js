85d7837f88bf0f7bf01f343150f73045570cba7b
07arif <ai.arif0711@gmail.com>
2024-01-18 12:11:31 +0600

    Math, abs, pow, round, ceil, floor, and random

---
+++ js-string-problems-practice/math.js
@@ -0,0 +1,43 @@
+const result = Math.pow(3, 8);
+console.log(result);
+
+// 1st number jodi big hoy thle kaj korbe na.
+//
+const num1 = 33;
+const num2 = 45;
+
+const gap = num1 - num2;
+if (gap < 5) {
+  console.log("YOu guys can be friends");
+} else {
+  console.log("you guys stay apart");
+}
+// console.log(gap);
+
+// --------------
+//
+
+const number = 2.5598;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+
+const result2 = Math.ceil(2.000001);
+console.log(result2);
+
+const result3 = Math.floor(2.9988);
+console.log(result3);
+
+// Random:
+
+// console.log(Math.random());
+const random = Math.random() * 100;
+console.log(random);
+
+const random1 = Math.round(Math.random() * 6);
+console.log(random1);
+
+//
+for (let i = 0; i < 20; i++) {
+  const random3 = Math.round(Math.random() * 6);
+  console.log(random3);
+}
