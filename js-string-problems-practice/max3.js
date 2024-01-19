82802495156ae260261c5f3bc1ce038421eb9d8c
07arif <ai.arif0711@gmail.com>
2024-01-19 15:39:36 +0600

    who will get the cake

---
+++ js-string-problems-practice/max3.js
@@ -0,0 +1,16 @@
+const jim = 184;
+const dela = 99;
+const chinku = 277;
+
+if (jim > dela && jim > chinku) {
+  console.log("jim will get the cake ");
+} else if (dela > jim && dela > chinku) {
+  console.log("dela will get the cake");
+} else {
+  console.log("chinku will get the cake");
+}
+
+Math.max(45, 98, 96);
+
+//Write a function that will take 3 numbers will return the max number
+//Write a function that will take 3 numbers will return the min number
