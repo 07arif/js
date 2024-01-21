2cb13fb740f2994e07ff8c4b4195f87d8186d6f7
07arif <ai.arif0711@gmail.com>
2024-01-22 00:10:48 +0600

    get part of an array and insert elements using slice, splice

---
+++ explore-more-javascrift-problems/splice.js
@@ -0,0 +1,12 @@
+const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
+// Removes elements from an array
+// and if necessary, inserts new elements in their place,
+// returning the deleted elements.w
+// will change the original array
+//
+// const partial = friends.splice(2, 3);
+// for add some number
+const partial1 = friends.splice(2, 3, 99, 44, 44, 23, 23, 1000);
+// console.log(partial);
+console.log(partial1);
+console.log(friends);
