311a8b6f5d0c8eedd0881bac9b4c9dfefd0127f1
07arif <ai.arif0711@gmail.com>
2024-01-24 23:59:28 +0600

    use add and multiplication to calculate wood requirments

--- explore-more-javascrift-problems/splice.js
+++ explore-more-javascrift-problems/splice.js
@@ -8,5 +8,6 @@ const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
 // for add some number
 const partial1 = friends.splice(2, 3, 99, 44, 44, 23, 23, 1000);
 // console.log(partial);
-console.log(partial1);
+// console.log(partial1);
 console.log(friends);
+//
