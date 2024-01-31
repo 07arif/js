6e9443fc5c12b8a44f91e1892be08f430a25bc78
07arif <ai.arif0711@gmail.com>
2024-01-31 23:57:57 +0600

    basic concept

---
+++ explore-more-javascrift-problems/summary.js
@@ -0,0 +1,14 @@
+/*
+Javascript is a high-level, interpreted programming language It makes the web pages 
+more interactive. we can use javascript to make animation.
+
+variable is like a box that store things. there are five types of variables number,
+string boolean, null and undefined 
+
+    
+    
+
+
+
+
+*/
