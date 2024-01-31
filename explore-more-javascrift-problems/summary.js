680738b21e6b9694af56266378319a35871d5dd3
07arif <ai.arif0711@gmail.com>
2024-02-01 00:01:33 +0600

    basic js checklist

--- explore-more-javascrift-problems/summary.js
+++ explore-more-javascrift-problems/summary.js
@@ -5,6 +5,15 @@ more interactive. we can use javascript to make animation.
 variable is like a box that store things. there are five types of variables number,
 string boolean, null and undefined 
 
+javascript keywords are reserved words and they cannot be used as variable name and 
+function name.
+
+camel case, snake case, and pascal case are the three most common naming conventions
+
+variable names are case sensitive.
+
+
+
     
     
 
