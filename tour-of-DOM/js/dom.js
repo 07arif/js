cbb4c8281a8ec0775917998bcaf52737ae40d718
07arif <ai.arif0711@gmail.com>
2024-02-03 21:36:58 +0600

    Traversing Dom - getElementByTagName

--- tour-of-DOM/js/dom.js
+++ tour-of-DOM/js/dom.js
@@ -1,2 +1,15 @@
 console.log("hello");
 console.log(document.body);
+
+// document.getElementByTagName:
+const liCollection = document.getElementsByTagName("li");
+console.log(liCollection);
+for (const li of liCollection) {
+  console.log("By tagName:", li.innerText);
+}
+
+// c
+const allHeadings = document.getElementsByTagName("h1");
+for (const h1 of allHeadings) {
+  console.log(h1.innerText);
+}
