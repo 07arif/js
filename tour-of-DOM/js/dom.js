da09ab1d8aa16c980bf00d437eb7d568df1180df
07arif <ai.arif0711@gmail.com>
2024-02-03 22:16:32 +0600

    Traversing DOM- getElementByClass vs GetElementById

--- tour-of-DOM/js/dom.js
+++ tour-of-DOM/js/dom.js
@@ -8,8 +8,23 @@ for (const li of liCollection) {
   console.log("By tagName:", li.innerText);
 }
 
-// c
+// document.getElementByTagName
 const allHeadings = document.getElementsByTagName("h1");
 for (const h1 of allHeadings) {
   console.log(h1.innerText);
+}
+
+// document.getElementById:
+const fruitsTitle = document.getElementById("fruits-title");
+console.log(fruitsTitle);
+fruitsTitle.innerText = "Fruits Changed by JS";
+
+// document.getElementById:
+const placesTitle = document.getElementById("places-title");
+console.log(placesTitle);
+
+//document.getElementByClassName:
+const importantPlaces = document.getElementsByClassName("important-places");
+for (const place of importantPlaces) {
+  console.log(place.innerText);
 }
