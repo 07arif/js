04fa883f94b13cc23686097c916a0becc420b654
07arif <ai.arif0711@gmail.com>
2024-02-16 21:21:00 +0600

    add event listener and compare email password

---
+++ baper-bank/js/login.js
@@ -0,0 +1,20 @@
+//  step:1- add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function () {
+  // step:2- get the email address inside the email input field
+  const emailField = document.getElementById('email-field');
+  const email = emailField.value;
+  // console.log(email);
+  // step:3- get the password  inside the password input field
+  // 3.a: set id on the html element
+  // 3.b: get the element
+  // 3.c: get the value from the element
+  const passwordField = document.getElementById('password-field');
+  const password = passwordField.value;
+  // console.log(password);
+  //DANGER::: DO NOT VERIFY EMAIL PASSWORD ON THE CLIENT SIDE.
+  if (email === 'sontan@bap.com' && password === 'secret') {
+    console.log('valid user');
+  } else {
+    console.log('invalid user');
+  }
+});
