50932e63f0b026e54ba1b75ed375216d0b9e107c
07arif <ai.arif0711@gmail.com>
2024-02-26 23:14:37 +0600

    Recap update deposit and balance and read code

--- baap-er-bank-recap/js/deposit.js
+++ baap-er-bank-recap/js/deposit.js
@@ -8,6 +8,10 @@
 4.get the previous deposit total
 
 5. calculate new deposit total and set the value to the deposit total
+
+6. get current balance
+
+7. calculate the new balance and set it to the balance total element
 */
 document.getElementById('btn-deposit').addEventListener('click', function () {
   const depositField = document.getElementById('deposit-field');
@@ -26,4 +30,13 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
   // step-5
   const newDepositTotal = previousDepositTotal + newDepositAmount;
   depositTotalElement.innerText = newDepositTotal;
+
+  // step-6
+  const balanceTotalElement = document.getElementById('balance-total');
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+  // step-7
+  const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+  balanceTotalElement.innerText = newBalanceTotal;
 });
---
+++ baap-er-bank-recap/js/withdraw.js
@@ -0,0 +1,7 @@
+/*
+add event handler with the withdraw button
+
+*/
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+  console.log('clicked');
+});
