2287883d4e23245e3b42a820a8596a75396e7d6c
07arif <ai.arif0711@gmail.com>
2024-02-27 23:59:49 +0600

    money withdraw and reduce

--- baap-er-bank-recap/js/withdraw.js
+++ baap-er-bank-recap/js/withdraw.js
@@ -1,7 +1,37 @@
 /*
-add event handler with the withdraw button
-
+1.add event handler with the withdraw button
+2. get the withdraw amount
+3.clear the withdraw input field
+4.get previous withdraw total
+5.calculate total withdraw amount and set it to the withdraw total element
+6. get previous balance
+7. calculate new balance and set it to the balance element
+ 
 */
+// step:1
 document.getElementById('btn-withdraw').addEventListener('click', function () {
-  console.log('clicked');
+  // step-2:
+  const withdrawField = document.getElementById('withdraw-field');
+  const newWithdrawAmountString = withdrawField.value;
+  const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+  // console.log(newWithdrawAmountString);
+  // step-3
+  withdrawField.value = '';
+  // step-4
+  const withdrawTotalElement = document.getElementById('withdraw-total');
+  const previousWithdrawTotalString = withdrawTotalElement.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+  // step-5
+  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = newWithdrawTotal;
+
+  // sep-6
+  const balanceElement = documentt.gettElementById('balance-total');
+  const previousBalanceTotalString = balanceElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  // step 7
+  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+  balanceElement.innerText = newBalanceTotal;
 });
