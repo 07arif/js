bcf088922bb4f61de68b926da7cdc4a297a31177
07arif <ai.arif0711@gmail.com>
2024-03-01 23:12:29 +0600

    reduse

--- functional-baap-er-bank/js/deposit.js
+++ functional-baap-er-bank/js/deposit.js
@@ -1,1 +1,9 @@
-console.log(utilitiesFile);
+document.getElementById('btn-deposit').addEventListener('click', function () {
+  /*
+    1.get the element by id
+    2.get the value from the element
+    3.convert string value to a number
+    
+    
+    */
+});
